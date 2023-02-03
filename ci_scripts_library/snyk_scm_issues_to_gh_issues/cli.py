2dafe51df0dc937e451797131425abcb888fb75b
roberthicks <roberthicksiii@gmail.com>
2023-02-03 14:08:49 -0500

    Updating GH action

--- ci_scripts_library/snyk_scm_issues_to_gh_issues/cli.py
+++ ci_scripts_library/snyk_scm_issues_to_gh_issues/cli.py
@@ -73,9 +73,10 @@ def main(ctx: typer.Context,
     g['github_client'] = GitHub(g['github_token'])
     typer.echo("Github client created successfully")
 
-    typer.echo("snyk token " + g['snyk_token'])
     g['snyk_client'] = SuperSnykClient(g['snyk_token'])
     typer.echo("Snyk client created successfully")
+
+    typer.echo("Remote repo url" + g['remote_repo_url'])
 
     g['github_org'] = get_github_org_name(remote_repo_url)
     g['snyk_org'] = find_snyk_org_from_github_org(g['snyk_client'], g['github_org'], g['snyk_prefix'])
