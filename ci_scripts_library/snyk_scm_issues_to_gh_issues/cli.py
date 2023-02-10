4cd0c0fd5a725ea881fd2458790110e4df191b5c
roberthicks <roberthicksiii@gmail.com>
2023-02-09 20:06:00 -0500

    Updating GitHub actions for env

--- ci_scripts_library/snyk_scm_issues_to_gh_issues/cli.py
+++ ci_scripts_library/snyk_scm_issues_to_gh_issues/cli.py
@@ -85,9 +85,11 @@ def main(ctx: typer.Context,
     # g['snyk_client'] = SuperSnykClient(snyk_t)
     # typer.echo("Snyk client created successfully")
 
-    typer.echo("Remote repo url " + g['remote_repo_url'])
+    typer.echo("Remote repo url " + g['github_org'])
 
     g['github_org'] = get_github_org_name(g['remote_repo_url'])
+
+    typer.echo("Github Org name " + g['github_org'])
     # g['github_org'] = "rhicksiii91"
     # g['snyk_org'] = find_snyk_org_from_github_org(g['snyk_client'], g['github_org'], g['snyk_prefix'])
     g['snyk_org'] = find_snyk_org_from_github_org(g['snyk_client'], g['github_org'])
