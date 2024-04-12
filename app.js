a8260f792b785f293accc2c432065a698617f905
Zelalem Mekonen <zola@programmer.net>
2024-04-12 10:06:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ cons.dust.helpers = dustHelpers;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(logger('dev'));
-app.use(methodOverride());
 app.use(session({
   secret: 'keyboard cat',
   name: 'connect.sid',
