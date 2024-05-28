a61cd040b624ead48a2a9f832333a4cc1f131f88
Allison Tearjen <altearjen@gmail.com>
2024-05-27 20:13:06 -0400

    add account detail

--- routes/index.js
+++ routes/index.js
@@ -112,6 +112,32 @@ exports.save_account_details = function(req, res, next) {
   }
 }
 
+exports.save_account_detail = function(req, res, next) {
+  // get the profile details from the JSON
+	const profile = req.body
+  // validate the input
+  if (validator.isEmail(profile.email, { allow_display_name: true })
+    // allow_display_name allows us to receive input as:
+    // Display Name <email-address>
+    // which we consider valid too
+    && validator.isMobilePhone(profile.phone, 'he-IL')
+    && validator.isAscii(profile.firstname)
+    && validator.isAscii(profile.lastname)
+    && validator.isAscii(profile.country)
+  ) {
+    // trim any extra spaces on the right of the name
+    profile.firstname = validator.rtrim(profile.firstname)
+    profile.lastname = validator.rtrim(profile.lastname)
+
+    // render the view
+    return res.render('account.hbs', profile)
+  } else {
+    // if input validation fails, we just render the view as is
+    console.log('error in form details')
+    return res.render('account.hbs')
+  }
+}
+
 exports.isLoggedIn = function (req, res, next) {
   if (req.session.loggedIn === 1) {
     return next()
