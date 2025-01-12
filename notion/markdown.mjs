99f42a0f80f76d659adcbfdc63b3e316b97bb0ce
kioxhiro <kio.hirokawa@sansan.com>
2025-01-12 16:48:12 +0900

    Update markdown.mjs

--- notion/markdown.mjs
+++ notion/markdown.mjs
@@ -3,6 +3,11 @@ import { readFileSync } from 'fs';
 import matter from 'gray-matter';
 import { markdownToBlocks } from '@tryfabric/martian';
 
+const BLOCK_ID = process.env.BLOCK_ID;
+const FILE_PATH = process.env.FILE_PATH;
+const TOKEN = process.env.TOKEN;
+
+
 console.log("tst")
 console.log(BLOCK_ID)
 console.log(TOKEN)
