dc8d92e46a57d1a57098a66906b3a8d9a47e676c
kioxhiro <kio.hirokawa@sansan.com>
2025-01-12 16:24:44 +0900

    Create markdown.js

---
+++ notion/markdown.js
@@ -0,0 +1,6 @@
+import { Client } from '@notionhq/client';
+import { readFileSync } from 'fs';
+import matter from 'gray-matter';
+import { markdownToBlocks } from '@tryfabric/martian';
+
+console.log("tst")
