c1cb10ea16097914fe0703f6483549ec730e8a3b
kioxhiro <kio.hirokawa@sansan.com>
2025-01-12 17:11:59 +0900

    Update markdown.mjs

--- notion/markdown.mjs
+++ notion/markdown.mjs
@@ -8,7 +8,7 @@ const FILE_PATH = process.env.FILE_PATH;
 const TOKEN = process.env.TOKEN;
 
 
-console.log("tst")
-console.log(`${process.env.BLOCK_ID}`)
-console.log(`${process.env.FILE_PATH}`)
+console.log("test")
+console.log('${process.env.BLOCK_ID}')
+console.log('${process.env.FILE_PATH}')
 console.log('${process.env.TOKEN}')
