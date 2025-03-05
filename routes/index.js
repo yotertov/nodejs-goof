7ee54332bb504becd4fb9d9eb0d50250c59f676d
Andrei-Serban Zaha <andrei.serbanzaha@snyk.io>
2025-03-05 11:50:44 +0200

    secret tokens

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,44 @@
-var utils = require('../utils');
-var mongoose = require('mongoose');
-var Todo = mongoose.model('Todo');
-var User = mongoose.model('User');
+var utils = require("../utils");
+var mongoose = require("mongoose");
+var Todo = mongoose.model("Todo");
+var User = mongoose.model("User");
 // TODO:
-var hms = require('humanize-ms');
-var ms = require('ms');
-var streamBuffers = require('stream-buffers');
-var readline = require('readline');
-var moment = require('moment');
-var exec = require('child_process').exec;
-var validator = require('validator');
+var hms = require("humanize-ms");
+var ms = require("ms");
+var streamBuffers = require("stream-buffers");
+var readline = require("readline");
+var moment = require("moment");
+var exec = require("child_process").exec;
+var validator = require("validator");
+
+var token0 = "SECRET_TOKEN_f8ed84e8f41e4146403dd4a6bbcea5e418d23a9";
+var token1 = "SECRET_TOKEN_f8ed84e8f41e4146403dd4a6bbcea5e418d23a9";
+var token2 = "SECRET_TOKEN_f8ed84e8f41e4146403dd4a6bbcea5e418d23a9";
+var token3 = "SECRET_TOKEN_f8ed84e8f41e4146403dd4a6bbcea5e418d23a9";
+var token4 = "SECRET_TOKEN_f8ed84e8f41e4146403dd4a6bbcea5e418d23a9";
+var token5 = "SECRET_TOKEN_f8ed84e8f41e4146403dd4a6bbcea5e418d23a9";
+var token6 = "SECRET_TOKEN_f8ed84e8f41e4146403dd4a6bbcea5e418d23a9";
+var token7 = "SECRET_TOKEN_f8ed84e8f41e4146403dd4a6bbcea5e418d23a9";
+var token8 = "SECRET_TOKEN_f8ed84e8f41e4146403dd4a6bbcea5e418d23a9";
+var token9 = "SECRET_TOKEN_f8ed84e8f41e4146403dd4a6bbcea5e418d23a9";
 
 // zip-slip
-var fileType = require('file-type');
-var AdmZip = require('adm-zip');
-var fs = require('fs');
+var fileType = require("file-type");
+var AdmZip = require("adm-zip");
+var fs = require("fs");
 
 // prototype-pollution
-var _ = require('lodash');
+var _ = require("lodash");
 
 exports.index = function (req, res, next) {
-  Todo.
-    find({}).
-    sort('-updated_at').
-    exec(function (err, todos) {
+  Todo.find({})
+    .sort("-updated_at")
+    .exec(function (err, todos) {
       if (err) return next(err);
 
-      res.render('index', {
-        title: 'Patch TODO List',
-        subhead: 'Vulnerabilities at their best',
+      res.render("index", {
+        title: "Patch TODO List",
+        subhead: "Vulnerabilities at their best",
         todos: todos,
       });
     });
@@ -36,114 +46,118 @@ exports.index = function (req, res, next) {
 
 exports.loginHandler = function (req, res, next) {
   if (validator.isEmail(req.body.username)) {
-    User.find({ username: req.body.username, password: req.body.password }, function (err, users) {
-      if (users.length > 0) {
-        const redirectPage = req.body.redirectPage
-        const session = req.session
-        const username = req.body.username
-        return adminLoginSuccess(redirectPage, session, username, res)
-      } else {
-        return res.status(401).send()
+    User.find(
+      { username: req.body.username, password: req.body.password },
+      function (err, users) {
+        if (users.length > 0) {
+          const redirectPage = req.body.redirectPage;
+          const session = req.session;
+          const username = req.body.username;
+          return adminLoginSuccess(redirectPage, session, username, res);
+        } else {
+          return res.status(401).send();
+        }
       }
-    });
+    );
   } else {
-    return res.status(401).send()
+    return res.status(401).send();
   }
 };
 
 function adminLoginSuccess(redirectPage, session, username, res) {
-  session.loggedIn = 1
+  session.loggedIn = 1;
 
   // Log the login action for audit
-  console.log(`User logged in: ${username}`)
+  console.log(`User logged in: ${username}`);
 
   if (redirectPage) {
-      return res.redirect(redirectPage)
+    return res.redirect(redirectPage);
   } else {
-      return res.redirect('/admin')
+    return res.redirect("/admin");
   }
 }
 
 exports.login = function (req, res, next) {
-  return res.render('admin', {
-    title: 'Admin Access',
+  return res.render("admin", {
+    title: "Admin Access",
     granted: false,
-    redirectPage: req.query.redirectPage
+    redirectPage: req.query.redirectPage,
   });
 };
 
 exports.admin = function (req, res, next) {
-  return res.render('admin', {
-    title: 'Admin Access Granted',
+  return res.render("admin", {
+    title: "Admin Access Granted",
     granted: true,
   });
 };
 
-exports.get_account_details = function(req, res, next) {
+exports.get_account_details = function (req, res, next) {
   // @TODO need to add a database call to get the profile from the database
   // and provide it to the view to display
-  const profile = {}
- 	return res.render('account.hbs', profile)
-}
+  const profile = {};
+  return res.render("account.hbs", profile);
+};
 
-exports.save_account_details = function(req, res, next) {
+exports.save_account_details = function (req, res, next) {
   // get the profile details from the JSON
-	const profile = req.body
+  const profile = req.body;
   // validate the input
-  if (validator.isEmail(profile.email, { allow_display_name: true })
+  if (
+    validator.isEmail(profile.email, { allow_display_name: true }) &&
     // allow_display_name allows us to receive input as:
     // Display Name <email-address>
     // which we consider valid too
-    && validator.isMobilePhone(profile.phone, 'he-IL')
-    && validator.isAscii(profile.firstname)
-    && validator.isAscii(profile.lastname)
-    && validator.isAscii(profile.country)
+    validator.isMobilePhone(profile.phone, "he-IL") &&
+    validator.isAscii(profile.firstname) &&
+    validator.isAscii(profile.lastname) &&
+    validator.isAscii(profile.country)
   ) {
     // trim any extra spaces on the right of the name
-    profile.firstname = validator.rtrim(profile.firstname)
-    profile.lastname = validator.rtrim(profile.lastname)
+    profile.firstname = validator.rtrim(profile.firstname);
+    profile.lastname = validator.rtrim(profile.lastname);
 
     // render the view
-    return res.render('account.hbs', profile)
+    return res.render("account.hbs", profile);
   } else {
     // if input validation fails, we just render the view as is
-    console.log('error in form details')
-    return res.render('account.hbs')
+    console.log("error in form details");
+    return res.render("account.hbs");
   }
-}
+};
 
 exports.isLoggedIn = function (req, res, next) {
   if (req.session.loggedIn === 1) {
-    return next()
+    return next();
   } else {
-    return res.redirect('/')
+    return res.redirect("/");
   }
-}
+};
 
 exports.logout = function (req, res, next) {
-  req.session.loggedIn = 0
-  req.session.destroy(function() { 
-    return res.redirect('/')  
-  })
-}
+  req.session.loggedIn = 0;
+  req.session.destroy(function () {
+    return res.redirect("/");
+  });
+};
 
 function parse(todo) {
   var t = todo;
 
-  var remindToken = ' in ';
+  var remindToken = " in ";
   var reminder = t.toString().indexOf(remindToken);
   if (reminder > 0) {
     var time = t.slice(reminder + remindToken.length);
-    time = time.replace(/\n$/, '');
+    time = time.replace(/\n$/, "");
 
     var period = hms(time);
 
-    console.log('period: ' + period);
+    console.log("period: " + period);
 
     // remove it
     t = t.slice(0, reminder);
-    if (typeof period != 'undefined') {
-      t += ' [' + ms(period) + ']';
+    if (typeof period != "undefined") {
+      t += " [" + ms(period) + "]";
     }
   }
   return t;
@@ -154,17 +168,16 @@ exports.create = function (req, res, next) {
 
   var item = req.body.content;
   var imgRegex = /\!\[alt text\]\((http.*)\s\".*/;
-  if (typeof (item) == 'string' && item.match(imgRegex)) {
+  if (typeof item == "string" && item.match(imgRegex)) {
     var url = item.match(imgRegex)[1];
-    console.log('found img: ' + url);
+    console.log("found img: " + url);
 
-    exec('identify ' + url, function (err, stdout, stderr) {
+    exec("identify " + url, function (err, stdout, stderr) {
       console.log(err);
       if (err !== null) {
-        console.log('Error (' + err + '):' + stderr);
+        console.log("Error (" + err + "):" + stderr);
       }
     });
-
   } else {
     item = parse(item);
   }
@@ -180,8 +193,8 @@ exports.create = function (req, res, next) {
     res.redirect('/');
     */
 
-    res.setHeader('Location', '/');
-    res.status(302).send(todo.content.toString('base64'));
+    res.setHeader("Location", "/");
+    res.status(302).send(todo.content.toString("base64"));
 
     // res.redirect('/#' + todo.content.toString('base64'));
   });
@@ -189,58 +202,53 @@ exports.create = function (req, res, next) {
 
 exports.destroy = function (req, res, next) {
   Todo.findById(req.params.id, function (err, todo) {
-
     try {
       todo.remove(function (err, todo) {
         if (err) return next(err);
-        res.redirect('/');
+        res.redirect("/");
       });
-    } catch (e) {
-    }
+    } catch (e) {}
   });
 };
 
 exports.edit = function (req, res, next) {
-  Todo.
-    find({}).
-    sort('-updated_at').
-    exec(function (err, todos) {
+  Todo.find({})
+    .sort("-updated_at")
+    .exec(function (err, todos) {
       if (err) return next(err);
 
-      res.render('edit', {
-        title: 'TODO',
+      res.render("edit", {
+        title: "TODO",
         todos: todos,
-        current: req.params.id
+        current: req.params.id,
       });
     });
 };
 
 exports.update = function (req, res, next) {
   Todo.findById(req.params.id, function (err, todo) {
-
     todo.content = req.body.content;
     todo.updated_at = Date.now();
     todo.save(function (err, todo, count) {
       if (err) return next(err);
 
-      res.redirect('/');
+      res.redirect("/");
     });
   });
 };
 
 // ** express turns the cookie key to lowercase **
 exports.current_user = function (req, res, next) {
-
   next();
 };
 
 function isBlank(str) {
-  return (!str || /^\s*$/.test(str));
+  return !str || /^\s*$/.test(str);
 }
 
 exports.import = function (req, res, next) {
   if (!req.files) {
-    res.send('No files were uploaded.');
+    res.send("No files were uploaded.");
     return;
   }
 
@@ -256,30 +264,30 @@ exports.import = function (req, res, next) {
     var extracted_path = "/tmp/extracted_files";
     zip.extractAllTo(extracted_path, true);
     data = "No backup.txt file found";
-    fs.readFile('backup.txt', 'ascii', function (err, data) {
+    fs.readFile("backup.txt", "ascii", function (err, data) {
       if (!err) {
         data = data;
       }
     });
   } else {
-    data = importFile.data.toString('ascii');
+    data = importFile.data.toString("ascii");
   }
-  var lines = data.split('\n');
+  var lines = data.split("\n");
   lines.forEach(function (line) {
-    var parts = line.split(',');
+    var parts = line.split(",");
     var what = parts[0];
-    console.log('importing ' + what);
+    console.log("importing " + what);
     var when = parts[1];
     var locale = parts[2];
     var format = parts[3];
     var item = what;
     if (!isBlank(what)) {
       if (!isBlank(when) && !isBlank(locale) && !isBlank(format)) {
-        console.log('setting locale ' + parts[1]);
+        console.log("setting locale " + parts[1]);
         moment.locale(locale);
         var d = moment(when);
-        console.log('formatting ' + d);
-        item += ' [' + d.format(format) + ']';
+        console.log("formatting " + d);
+        item += " [" + d.format(format) + "]";
       }
 
       new Todo({
@@ -287,22 +295,21 @@ exports.import = function (req, res, next) {
         updated_at: Date.now(),
       }).save(function (err, todo, count) {
         if (err) return next(err);
-        console.log('added ' + todo);
+        console.log("added " + todo);
       });
     }
   });
 
-  res.redirect('/');
+  res.redirect("/");
 };
 
 exports.about_new = function (req, res, next) {
   console.log(JSON.stringify(req.query));
-  return res.render("about_new.dust",
-    {
-      title: 'Patch TODO List',
-      subhead: 'Vulnerabilities at their best',
-      device: req.query.device
-    });
+  return res.render("about_new.dust", {
+    title: "Patch TODO List",
+    subhead: "Vulnerabilities at their best",
+    device: req.query.device,
+  });
 };
 
 // Prototype Pollution
@@ -312,18 +319,18 @@ exports.about_new = function (req, res, next) {
 // same logic using MongoDB.
 const users = [
   // You know password for the user.
-  { name: 'user', password: 'pwd' },
+  { name: "user", password: "pwd" },
   // You don't know password for the admin.
-  { name: 'admin', password: Math.random().toString(32), canDelete: true },
+  { name: "admin", password: Math.random().toString(32), canDelete: true },
 ];
 
 let messages = [];
 let lastId = 1;
 
 function findUser(auth) {
-  return users.find((u) =>
-    u.name === auth.name &&
-    u.password === auth.password);
+  return users.find(
+    (u) => u.name === auth.name && u.password === auth.password
+  );
 }
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -335,13 +342,13 @@ exports.chat = {
     const user = findUser(req.body.auth || {});
 
     if (!user) {
-      res.status(403).send({ ok: false, error: 'Access denied' });
+      res.status(403).send({ ok: false, error: "Access denied" });
       return;
     }
 
     const message = {
       // Default message icon. Cen be overwritten by user.
-      icon: '👋',
+      icon: "👋",
     };
 
     _.merge(message, req.body.message, {
@@ -357,11 +364,11 @@ exports.chat = {
     const user = findUser(req.body.auth || {});
 
     if (!user || !user.canDelete) {
-      res.status(403).send({ ok: false, error: 'Access denied' });
+      res.status(403).send({ ok: false, error: "Access denied" });
       return;
     }
 
     messages = messages.filter((m) => m.id !== req.body.messageId);
     res.send({ ok: true });
-  }
+  },
 };
