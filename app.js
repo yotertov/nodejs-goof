c4243e94f2736e7b16fdcf2e76cb2a5b3270e734
tfn-snyk <thomas.nielson@snyk.io>
2025-05-28 10:02:23 -0600

    intro hardcoded secrets for fix AI example

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(methodOverride());
 app.use(session({
-  secret: process.env.SESSION_SECRET,
+  secret: 'keyboard cat',
   name: 'connect.sid',
   cookie: { path: '/' }
 }))
